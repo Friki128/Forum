b5197902df9fa7e99eb9dd6507f19b258f0d2c7d
Zelda <zeldagarrocho@gmail.com>
2025-02-19 08:17:11 +0100

    setup

---
+++ src/test/java/net/esliceu/Rest_Api_Forum/RestApiForumApplicationTests.java
@@ -0,0 +1,13 @@
+package net.esliceu.Rest_Api_Forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
